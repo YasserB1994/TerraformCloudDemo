da53830b3b6a5dc5264f3c59b533974ddbd42a75
EC2 Default User <ec2-user@ip-172-31-30-201.ec2.internal>
2022-10-31 23:52:37 +0000

    Updating TF file

--- Main.tf
+++ Main.tf
@@ -1,19 +1,6 @@
-variable "AWS_ACCESS_KEY" {
-  type = string
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
-}
-
-variable "AWS_REGION" {
-  type = string
-}
-
 provider "aws" {
-  region     = var.AWS_REGION
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region     = "eu-west-1"
+ 
 }
 
 module "s3_bucket" {
